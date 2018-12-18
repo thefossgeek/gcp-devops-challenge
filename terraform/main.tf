b5e4c652d7f062e1401716e17c964d8d6d27af46
Mohan L <thefossgeek@mail.com>
2018-12-19 02:07:39 +0530

    initial tf files..

---
+++ terraform/main.tf
@@ -0,0 +1,73 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = "${file("${var.credentials}")}"
+ project     = "${var.gcp_project}" 
+ region      = "${var.region}"
+}
+
+// Create VPC
+resource "google_compute_network" "vpc" {
+ name                    = "${var.name}-vpc"
+ auto_create_subnetworks = "false"
+}
+
+// Create Subnet
+resource "google_compute_subnetwork" "subnet" {
+ name          = "${var.name}-subnet"
+ ip_cidr_range = "${var.subnet_cidr}"
+ network       = "${var.name}-vpc"
+ depends_on    = ["google_compute_network.vpc"]
+ region      = "${var.region}"
+}
+
+// VPC firewall configuration
+resource "google_compute_firewall" "firewall" {
+  name    = "${var.name}-firewall"
+  network = "${google_compute_network.vpc.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+//resource "google_compute_address" "static" {
+//  count = "${var.amount}"
+//  name  = "${var.name}-${count.index}"
+//}
+
+resource "google_compute_instance" "vms" {
+  count                     = "${var.amount}"
+  name                      = "${var.name}-${count.index + 1}"
+  zone                      = "${var.zone}"
+  machine_type              = "${var.machine_type}"
+
+  boot_disk {
+    auto_delete = "${var.disk_auto_delete}"
+
+    initialize_params {
+      image = "${var.image_project}/${var.image_family}"
+      size  = "${var.disk_size_gb}"
+      type  = "${var.disk_type}"
+    }
+  }
+
+ metadata {
+   sshKeys = "${var.user}:${file("~/.ssh/id_rsa.pub")}"
+ }
+
+  network_interface {
+    subnetwork    = "${google_compute_subnetwork.subnet.name}"
+    access_config = {
+      //Ephemeral IP
+    }
+  }
+
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {}
+variable "zone" {}
+variable "gcp_project" {}
+variable "credentials" {}
+
+variable "amount" {}
+variable "name" {}
+variable "subnet_cidr" {}
+
+
+variable "machine_type" {}
+variable "disk_auto_delete" {}
+variable "image_family" {}
+variable "image_project" {}
+variable "disk_type" {}
+variable "disk_size_gb" {}
+variable "user" {}
